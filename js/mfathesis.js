b5dbd2e4ed9b50d7915222f6604222a2d728e165
Tim Lupfer <tim@timlupfer.com>
2014-02-21 17:49:58 -0600

    Use dummy data and start compiling student list

--- js/mfathesis.js
+++ js/mfathesis.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
   // Show a random header image
   $('header img.backgroundfill:nth-of-type('+_.random(1,3)+')').show();
+  
+  $.getJSON('http://mfa.cape.io/items/client_data.json', function(data) {
+    console.log(data);
+    var StudentTemplate = Hogan.compile($('#students-template').html());
+    console.log(StudentTemplate.render(data));
+    $('#projects-demo').html(StudentTemplate.render(data));
+  });
 });
