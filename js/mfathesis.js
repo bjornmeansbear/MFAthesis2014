daab66551da2fb44c55ccc15702635bbfa8a0878
Tim Lupfer <tim@timlupfer.com>
2014-02-21 19:17:59 -0600

    Real background images

--- js/mfathesis.js
+++ js/mfathesis.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   // Show a random header image
-  $('header img.backgroundfill:nth-of-type('+_.random(1,3)+')').show();
+  $('header img.backgroundfill:nth-of-type('+_.random(1,$('header img.backgroundfill').length)+')').show();
   
   $.getJSON('http://mfa.cape.io/items/client_data.json', function(data) {
     var StudentTemplate = Hogan.compile($('#students-template').html());
