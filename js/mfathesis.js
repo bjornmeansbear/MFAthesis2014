7ca40cfcc0b7332162a559850006bac6a93e94bd
Tim Lupfer <tim@timlupfer.com>
2014-02-21 18:30:37 -0600

    Index list with list.js

--- js/mfathesis.js
+++ js/mfathesis.js
@@ -3,9 +3,14 @@ $(document).ready(function() {
   $('header img.backgroundfill:nth-of-type('+_.random(1,3)+')').show();
   
   $.getJSON('http://mfa.cape.io/items/client_data.json', function(data) {
-    console.log(data);
     var StudentTemplate = Hogan.compile($('#students-template').html());
-    console.log(StudentTemplate.render(data));
     $('#projects-demo').html(StudentTemplate.render(data));
+    var options = {
+      valueNames: [ 'name', 'program' ]
+    };
+
+    var studentlist = new List('students', options);
+
+    console.log(studentlist.matchingItems);
   });
 });
